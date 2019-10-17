caa64f122f5365fb41d959fac0f4ff637f7b965a
Tomeek95 <34217893+Tomeek95@users.noreply.github.com>
2019-10-17 18:54:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// Slide-show -------------------------------------------------------------------------
+let index = 0;
+carousel();
+
+function carousel()
+{
+    let mySlides = document.getElementsByClassName("mySlides");
+
+    for (let i = 0; i < mySlides.length; i++) {
+        mySlides[i].style.display = "none";
+    }
+    index++;
+    
+    if(index > mySlides.length)
+    {
+        index = 1;
+    }
+    mySlides[index - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+};
+
+
+//Login------------------------------------------------------------------------------
+var modal = document.getElementById('id01');
+modal.style.display = "none";
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
